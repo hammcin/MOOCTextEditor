b1b5cebf3b1df85861f905d3b2dcaa39a886bd9b
Hamadi McIntosh <hamadi.mc@sbcglobal.net>
2020-07-03 10:24:12 -0400

    feat: Implement Dictionary using LinkedList

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ Flesch and it approximates how easy a piece of text is to read based on the
 number of sentences, words and syllables in that text.  Higher scores indicate
 text that is simple to read, while lower scores indicate more complex text.
 
+## Markov Text Generation
+
+Train a Markov Text Generator using the text in the editor as the source text.
+New text of any desired length that resembles the source text can then be
+generated using the trained Markov Text Generator.
+
 ## Acknowledgements
 
 [RichTextFX](https://github.com/TomasMikula/RichTextFX), which was created by
--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 
 /**
  * A class that implements the Dictionary interface using a LinkedList
+ * @author Hamadi McIntosh
  *
  */
 public class DictionaryLL implements Dictionary 
@@ -11,31 +12,35 @@ public class DictionaryLL implements Dictionary
 
 	private LinkedList<String> dict;
 	
-    // TODO: Add a constructor
+	public DictionaryLL() {
+		this.dict = new LinkedList<String>();
+	}
 
 
-    /** Add this word to the dictionary.  Convert it to lowercase first
-     * for the assignment requirements.
+    /** Add this word to the dictionary.  Convert it to lowercase first.
      * @param word The word to add
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+    	String wordLower = word.toLowerCase();
+    	if (!dict.contains(wordLower)) {
+    		dict.add(wordLower);
+    		return true;
+    	}
+    	return false;
     }
 
 
     /** Return the number of words in the dictionary */
     public int size()
     {
-        // TODO: Implement this method
-        return 0;
+    	return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-        //TODO: Implement this method
-        return false;
+    	String word = s.toLowerCase();
+        return dict.contains(word);
     }
 
     
